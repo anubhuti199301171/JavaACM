1852b85ed5dacb4c65410f937d163dd0d91260b3
anubhuti199301171 <56643076+anubhuti199301171@users.noreply.github.com>
2019-10-24 00:38:47 +0530

    Update Program3.java

--- Program3.java
+++ Program3.java
@@ -1,13 +1,16 @@
-import java.io*;
+import java.io.*;
 import java.util.*;
 class Program3
 {
 public static void main(String args[])throws IOException
 {
-Scanner sc=new Scanner(System.in);
-int n=0;
-float n1=0.0f;
-if(n==n1)System.out.println("equal");
-else System.out.println("not equal");
+
+Scanner sc = new Scanner(System.in);
+			int principal = sc.nextInt();
+			float rate = sc.nextFloat();
+			int time = sc.nextInt();
+			float SimpleInterest = principal*rate*time/100;
+			System.out.println("interest = "+ SimpleInterest);
+
 }
 }
