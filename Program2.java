f0e98f7f0c4095f1d836fe700a10c9919791cb88
anubhuti199301171 <56643076+anubhuti199301171@users.noreply.github.com>
2019-10-23 23:36:48 +0530

    Update Program2.java

--- Program2.java
+++ Program2.java
@@ -4,9 +4,10 @@ class Program2
 {
 	public static void main(String args[]) throws IOException
 	{
-		int n=3;
-		double n1=3;
+		int n=2;
+		double n1=2;
 		System.out.println(n);
-    System.out.println(n1);
+    		System.out.println(n1);
+		System.out.println("Now you know the difference between int and double ");
 	}
 }
